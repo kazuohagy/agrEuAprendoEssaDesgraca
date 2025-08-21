5f70e123d499f6855c966069283b84ac77150f87
Gustavo Kazuo <kazuo.hagy@gmail.com>
2025-08-21 11:06:39 -0300

    mario

---
+++ Controllers/UsuarioController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace fiapstore2.Controllers
+{
+    [ApiController]
+    [Route("Usuario")]
+    public class UsuarioController : ControllerBase
+    {
+        [HttpGet("obter-usuario")]
+        public IActionResult Obterusuario()
+        {
+            return Ok("Usuário obtido com sucesso!");
+        }
+        [HttpPost]
+        public IActionResult Criarusuario()
+        {
+            return Ok("Usuário criado com sucesso!");
+        }
+        [HttpPut]
+        public IActionResult Atualizarusuario()
+        {
+            return Ok("Usuário atualizado com sucesso!");
+        }
+        [HttpDelete]
+        public IActionResult Deletarusuario()
+        {
+            return Ok("Usuário deletado com sucesso!");
+        }
+    }
+}
